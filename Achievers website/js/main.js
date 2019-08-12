a636f0fbc91c2c478a1b3eacc53c1d8f6d77c0d4
prianshi <48873727+prianshi@users.noreply.github.com>
2019-08-12 12:57:37 +0530

    Add files via upload

---
+++ Achievers website/js/main.js
@@ -0,0 +1,21 @@
+window.onscroll=function(){stickyNav()};
+var navbar =document.getElementById('navbar');
+var sticky=navbar.offsetTop;
+function stickyNav(){
+    if(window.pageYOffset >= sticky){
+        navbar.classList.add('sticky');
+    }else{
+        navbar.classList.remove('sticky');
+    }
+}
+
+$(document).ready(function(){
+    $('.burger').click(function(){
+        $('ul').toggleClass('active')
+    })
+})
+
+
+
+
+
